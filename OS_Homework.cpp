d337bfee8a99975ef281bc4186882fc131176d2d
TofuNotHere <TofuNotHere@users.noreply.github.com>
2016-03-30 19:21:19 +0800

    Added files via upload

---
+++ OS_Homework.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+
+using namespace std;
+typedef LARGE_INTEGER ll;
+const int maxn = 500;
+const int base = 2e4;
+
+void run(int k){
+	while (k--){k--;k++;}
+}
+
+DWORD WINAPI AssThread(LPVOID argv)
+{
+	for (int i = 1; i < maxn*2; i++){
+		run(i*base);
+	}
+	return 0;
+}
+
+void OutputThread()
+{
+	ll begin_time, end_time;
+	double time_fre;
+	double time_dif;
+	ll litmp;
+	QueryPerformanceFrequency(&litmp);
+	time_fre = (double)litmp.QuadPart;
+	for (int i = 1; i < maxn; i++){
+		Sleep(0);
+		QueryPerformanceCounter(&begin_time);
+		run(i*base);
+		QueryPerformanceCounter(&end_time);
+		time_dif = (end_time.QuadPart - begin_time.QuadPart) / time_fre * 1000;
+		printf("%llf\n", time_dif);
+	}
+}
+
+void MT(){
+	HANDLE Thread;
+	DWORD ThreadID;
+	for (int i = 0; i < 16; i++)
+		Thread = CreateThread(0, 0, AssThread, 0, 0, &ThreadID);
+}
+
+int main(){
+	freopen("out.txt", "w", stdout);
+	MT();
+	OutputThread();
+	MT();
+	return 0;
+}
